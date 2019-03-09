1f424531ca17bc4c0359384b27d1280e44a289dc
tmfdk333 <tmfdk333@gmail.com>
2019-03-09 22:57:01 +0900

    chapter2 - 포커스 에디터

---
+++ src/main/java/com/tmfdk333/inflearn/chap2/editor/FocusEditor.java
@@ -0,0 +1,44 @@
+package com.tmfdk333.inflearn.chap2.editor;
+
+public class FocusEditor {
+    public void focus() {
+        String s = "안녕하세요. 반갑습니다. IntelliJ IDEA Community 2018 Version";
+        System.out.println(s);
+    }
+    public void page1() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page2() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page3() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page4() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page5() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page6() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page7() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page8() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+    public void page9() {
+        System.out.println("간격을 넓히기 위한 코드1");
+        System.out.println("간격을 넓히기 위한 코드2");
+    }
+}
